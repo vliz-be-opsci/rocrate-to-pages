9a5b4a266310a5ee21b1fba4013f9c4daf5cee2a
Rory <rory.meyer@vliz.be>
2022-03-24 14:06:08 +0100

    Added some HTML gen capabilities

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,11 @@
 # If you need Python 3 and the GitHub CLI, then use:
-FROM cicirello/pyaction:4
-
-# If all you need is Python 3, use:
-# FROM cicirello/pyaction-lite:3
-
-# If Python 3 + git is sufficient, then use:
-# FROM cicirello/pyaction:3
-
-# To pull from the GitHub Container Registry instead, use one of these:
-# FROM ghcr.io/cicirello/pyaction-lite:3
-# FROM ghcr.io/cicirello/pyaction:4
-# FROM ghcr.io/cicirello/pyaction:3
+FROM alpine:3.15.1
+RUN apk --no-cache add python3
+RUN apk --no-cache add npm
+RUN apk add --update py-pip
 
 COPY requirements.txt /requirements.txt 
 RUN pip install -r /requirements.txt
+RUN npm install ro-crate-html-js --global
 COPY entrypoint.py /entrypoint.py
 ENTRYPOINT ["/entrypoint.py"]
--- entrypoint.py
+++ entrypoint.py
@@ -12,6 +12,7 @@
     # - check validation?
 import sys
 import os
+import subprocess
 import logging 
 
 log = logging.getLogger('entrypoint') 
@@ -19,10 +20,13 @@ log = logging.getLogger('entrypoint')
 class CrateObj():
     def __init__(self, crate_dir):
         self.crate_dir = crate_dir
-        self.metadata_path = os.path.join(self.crate_dir, 'ro-crate-metadata.jsonld') 
-        self.metadata_exists = False
+        self.metadata_path = os.path.join(self.crate_dir, 'ro-crate-metadata')  
+        self.metadata_exists = False 
         self.preview_path  = os.path.join(self.crate_dir, 'ro-crate-preview.html')
         self.preview_exists = False
+        self.crate_valid = None
+        self.metadata_valid = None
+        self.preview_valid = None
 
         # TODO:
         # self.preview_valid = False
@@ -31,19 +35,25 @@ class CrateObj():
     def check_rocrate_valid(self):
         # Checks if there are rocrate objects in directory
         log.debug('Checking that rocrate files exist...') 
-        if os.path.exists(self.metadata_path):
-            log.info('ROCrate metadata file exists: {0}'.format(self.metadata_path))
+        if os.path.exists(os.path.join(self.metadata_path,'.json')):
+            log.info('ROCrate metadata json file exists: {0}'.format(self.metadata_path))
+            self.metadata_path = os.path.join(self.metadata_path,'.json')
+            self.metadata_exists = True
+        elif os.path.exists(os.path.join(self.metadata_path,'.jsonld')):
+            log.info('ROCrate metadata jsonld file exists: {0}'.format(self.metadata_altpath))
+            self.metadata_path = os.path.join(self.metadata_path,'.jsonld')
             self.metadata_exists = True
         else:
             log.error('ROCrate metadata file DOES NOT exist: {0}'.format(self.metadata_path))
             self.metadata_exists = False
+            self.crate_valid = False
+            exit(1)
 
         if os.path.exists(self.preview_path):
             log.info('ROCrate preview file exists: {0}'.format(self.preview_path))
             self.preview_exists = True
         else: 
-            log.warning('ROCrate metadata file exists: {0}'.format(self.metadata_path))
-            log.warning('Creating default preview file...')
+            log.warning('ROCrate preview file DOES NOT exist: {0}'.format(self.preview_path)) 
             self.preview_exists = False
             #TODO Create default html file here. 
 
@@ -51,13 +61,41 @@ class CrateObj():
         # --------------
         # TODO 
         # --------------
-        return 0
+        # Some Test:
+        self.check_metadata()
+        self.check_preview()
+        if self.metadata_valid and self.preview_valid:
+            log.info('Crate passes validity checks.')
+            self.crate_valid = True
+        return 
+    
+    def check_metadata(self):
+        log.info('Checking if metadata is valid...')
+        #some test
+        self.metadata_valid = True
+        return
+
+    def check_preview(self):
+        log.info('Checking if preview is valid...')
+        #some test
+        self.preview_valid = True
+        return
 
 def create_symlink(dst, src):
     # This creates a symbolic link on python in tmp directory
     log.debug(f'Creating symlink between {src} and {dst}')
     os.symlink(src, dst)
     return
+
+def create_preview_html(crate_obj):
+    ''' 
+    This uses https://github.com/UTS-eResearch/ro-crate-html-js to create a preview.html 
+    from a rocrate json file. 
+
+    rochtml rocrate_datacrate_test/ro-crate-metadata.json
+    '''
+    metadata_file = crate_obj.metadata_path
+    subprocess.check_call(f'rochtml {metadata_file}', shell=True)
 
 def publish_rocrate(crate_dir): 
     # steps to follow to create the correct files to publish to GH-Pages
@@ -68,9 +106,16 @@ def publish_rocrate(crate_dir):
 
     if this_crate.preview_exists:
         create_symlink(os.path.join(crate_dir, 'index.html'), this_crate.preview_path)
+    else: 
+        #Create index.html page
+        pass
+
     if this_crate.metadata_exists:
-        create_symlink(os.path.join(crate_dir, 'ro-crate-metadata.json'), this_crate.metadata_path)
-    
+        ## Create symlink between the .json >> .jsonld file extensions depending on which exists
+        if os.path.splitext(this_crate.metadata_path) == '.json':
+            create_symlink(os.path.join(crate_dir, 'ro-crate-metadata.jsonld'), this_crate.metadata_path) 
+        elif os.path.splitext(this_crate.metadata_path) == '.jsonld':
+            create_symlink(os.path.join(crate_dir, 'ro-crate-metadata.json'), this_crate.metadata_path) 
     log.debug('ROCrate ready to publish')
 
 if __name__ == "__main__" :
