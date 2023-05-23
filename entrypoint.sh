78d2b849f48cc6e3472882d58e426cc8fbbbedd7
cedricd <cedricdecruw@gmail.com>
2023-05-23 20:00:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,8 @@ mkdir ./src/data
 
 #copy all files from ./github/workspace to  ./src/data including hidden files
 rsync --recursive --progress -avzh ./github/workspace/* ./src/data
+#copy the .git folder from ./github/workspace to ./src/data
+rsync --recursive --progress -avzh ./github/workspace/.git ./src/data/.git
 
 #install all requirements
 pip install -r requirements.txt
